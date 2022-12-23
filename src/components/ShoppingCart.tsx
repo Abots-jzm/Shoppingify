8854abb373a13399e90f740532b09f06bee63f35
abotstheboss <abolarinoluwatobistephen@gmail.com>
2022-12-23 03:51:32 +0100

    fix: cart animation

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -3,10 +3,15 @@ import { AnimatePresence, motion, Variants } from "framer-motion";
 import styled from "styled-components";
 import useScreenSize from "../hooks/useScreenSize";
 import { useAppSelector } from "../store/hooks";
+import sourceSVG from "../assets/source.svg";
 
 const ContainerVariants: Variants = {
 	hidden: {
 		opacity: 0,
+		transition: {
+			duration: 0.1,
+			when: "afterChildren",
+		},
 	},
 	visible: {
 		opacity: 1,
@@ -15,29 +20,19 @@ const ContainerVariants: Variants = {
 			when: "beforeChildren",
 		},
 	},
-	exit: {
-		opacity: 0,
-		transition: {
-			duration: 0.1,
-			when: "afterChildren",
-		},
-	},
 };
 
 const CartVariants: Variants = {
 	hidden: {
 		x: "100%",
+		transition: {
+			duration: 0.2,
+		},
 	},
 	visible: {
 		x: 0,
 		transition: {
 			duration: 0.3,
-		},
-	},
-	exit: {
-		x: "100%",
-		transition: {
-			duration: 0.2,
 		},
 	},
 };
@@ -47,11 +42,21 @@ function ShoppingCart() {
 	const { screenWidth } = useScreenSize();
 
 	return (
-		<AnimatePresence mode="popLayout">
+		<AnimatePresence initial={false}>
 			{(cartIsOpen || screenWidth > 900) && (
-				<Container variants={ContainerVariants} initial="hidden" animate="visible" exit="exit">
-					<div />
-					<motion.div variants={CartVariants}>ShoppingCart</motion.div>
+				<Container variants={ContainerVariants} initial="hidden" animate="visible" exit="hidden">
+					<div className="empty" />
+					<motion.div variants={CartVariants} className="cart">
+						<AddNewItem>
+							<div className="img">
+								<img src={sourceSVG} alt="source" />
+							</div>
+							<div className="right">
+								<div>Didn't find what you need?</div>
+								<button>Add item</button>
+							</div>
+						</AddNewItem>
+					</motion.div>
 				</Container>
 			)}
 		</AnimatePresence>
@@ -60,12 +65,58 @@ function ShoppingCart() {
 
 export default ShoppingCart;
 
+const AddNewItem = styled.div`
+	background-color: #80485b;
+	color: white;
+	font-weight: 700;
+	border-radius: 24px;
+	width: 100%;
+	display: flex;
+	padding: 1.7rem 2rem;
+	gap: 3rem;
+
+	.img {
+		position: relative;
+
+		img {
+			position: relative;
+			transform: scale(1.4);
+			bottom: 1.4rem;
+		}
+	}
+
+	.right {
+		display: flex;
+		flex-direction: column;
+		gap: 1.4rem;
+		align-items: flex-start;
+
+		button {
+			background-color: white;
+			color: #34333a;
+			box-shadow: 0 2px 12px rgba(0, 0, 0, 0.04);
+			border-radius: 12px;
+			padding: 1.1rem 2.95rem;
+			font-size: 1.4rem;
+		}
+	}
+`;
+
 const Container = styled(motion.div)`
-	flex: 0 0 35rem;
+	flex: 0 0 39rem;
 	position: sticky;
 	top: 0;
 	height: 100vh;
 	background-color: rgba(0, 0, 0, 0.5);
+
+	.empty {
+		display: none;
+	}
+
+	.cart {
+		overflow-y: auto;
+		padding: 4rem 3.5rem;
+	}
 
 	& > div {
 		background-color: #fff0de;
@@ -79,22 +130,24 @@ const Container = styled(motion.div)`
 		inset: 0;
 		justify-content: space-between;
 
-		div:first-child {
+		.empty {
+			display: block;
 			flex: 0 0 9.3rem;
 		}
 
-		div:last-child {
+		.cart {
 			flex: 0 1 35rem;
 		}
 	}
 
 	@media only screen and (max-width: 600px) {
-		div:first-child {
+		.empty {
 			flex: 0 0 6.2rem;
 		}
 
-		div:last-child {
+		.cart {
 			flex: 0 1 31.5rem;
+			padding: 2.4rem 1.5rem;
 		}
 	}
 `;
